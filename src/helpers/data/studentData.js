d4ccc686199f96b0db06d0493d430c858435c99f
Bailey <bailey.elise.dennis@gmail.com>
2020-11-09 21:37:46 -0600

    pausing for the night...

---
+++ src/helpers/data/studentData.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+
+export default class studentData extends Component {
+  students = [
+    {
+      id: '1',
+      firstName: 'Autumn',
+      lastName: 'F',
+      isDead: false,
+    },
+    {
+      id: '2',
+      firstName: 'Bailey',
+      lastName: 'D',
+      isDead: false,
+    },
+    {
+      id: '3',
+      firstName: 'Chris',
+      lastName: 'L',
+      isDead: false,
+    },
+    {
+      id: '4',
+      firstName: 'Chris',
+      lastName: 'C',
+      isDead: false,
+    },
+    {
+      id: '5',
+      firstName: 'Dana',
+      lastName: 'P',
+      isDead: false,
+    },
+    {
+      id: '6',
+      firstName: 'Deanna',
+      lastName: 'M',
+      isDead: false,
+    },
+    {
+      id: '7',
+      firstName: 'Hunter',
+      lastName: 'J',
+      isDead: false,
+    },
+    {
+      id: '8',
+      firstName: 'Jonathon',
+      lastName: 'J',
+      isDead: false,
+    },
+    {
+      id: '9',
+      firstName: 'Jordan',
+      lastName: 'S',
+      isDead: false,
+    },
+    {
+      id: '10',
+      firstName: 'Joseph',
+      lastName: 'M',
+      isDead: false,
+    },
+    {
+      id: '11',
+      firstName: 'Kaitlyn',
+      lastName: 'V',
+      isDead: false,
+    },
+    {
+      id: '12',
+      firstName: 'Liz',
+      lastName: 'B',
+      isDead: false,
+    },
+    {
+      id: '13',
+      firstName: 'Michael',
+      lastName: 'P',
+      isDead: false,
+    },
+    {
+      id: '14',
+      firstName: 'Robert',
+      lastName: 'B',
+      isDead: false,
+    },
+    {
+      id: '15',
+      firstName: 'Ryan',
+      lastName: 'M',
+      isDead: false,
+    },
+    {
+      id: '16',
+      firstName: 'Samuel',
+      lastName: 'M',
+      isDead: false,
+    },
+    {
+      id: '17',
+      firstName: 'Summer',
+      lastName: 'D',
+      isDead: false,
+    },
+    {
+      id: '18',
+      firstName: 'Wanda',
+      lastName: 'M',
+      isDead: false,
+    },
+    {
+      id: '19',
+      firstName: 'Wendell',
+      lastName: 'P',
+      isDead: false,
+    },
+    {
+      id: '20',
+      firstName: 'Will',
+      lastName: 'K',
+      isDead: false,
+    },
+  ];
+
+  livingStudents = () => {
+    const livingStudents = this.students.filter(
+      (student) => student.isDead === false
+    );
+    return livingStudents;
+  };
+
+  dearlyBeloved = () => {
+    const ripStudents = this.students.filter(
+      (student) => student.isDead !== false,
+    );
+    return ripStudents;
+  };
+}
